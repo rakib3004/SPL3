67be2310abf2ef654fb5d238bb5eab783f46c23c
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-12-03 17:12:07 +0600

    initiate react front end

---
+++ LuminousFront/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './vite.svg'; // Assuming the Vite logo is in the same directory as the TypeScript file
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+        <h1>Hi</h1>
+    </div>
+  );
+}
+
+export default App;
