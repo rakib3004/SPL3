77463c0c126276ff2431f9a669febf714343308c
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-12-11 03:44:46 +0600

    complete initial frontend using angular

--- harmony-ui/src/app/app.routes.ts
+++ harmony-ui/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home/home.component';
+import { TranslationComponent } from './translation/translation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'translation/:type', component: TranslationComponent },
+];
---
+++ harmony-ui/src/app/welcome/welcome.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent {
+
+  constructor(private router: Router) {}
+
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
+}
